51088eddeb1dbf9b3bebf1e7f88bde10f53ea41b
memthopu <memthopu@memthopus-MacBook-Air.local>
2026-05-04 22:15:38 -0400

    session 02 - primitive data types and identifiers

---
+++ src/primitivetypes.js
@@ -0,0 +1,51 @@
+//1.numbers:
+let x = 10;
+x= 20;
+console.log(x);
+console.log(typeof x);
+
+let y = 12.33;
+console.log(y);
+console.log(typeof y);
+
+let z = -100;
+console.log(z);
+//2.String
+
+let name = "Tom";
+console.log(name);
+console.log(typeof name);
+
+let m = '$';
+console.log(m);
+console.log(typeof m);
+
+let s = '1000';
+console.log(s);
+console.log(typeof s);
+
+//3.undefined
+
+let p;
+console.log(p);
+console.log(typeof p);
+
+let firstNmae = undefined; // good practice
+console.log(firstNmae);
+console.log(typeof firstNmae);
+
+//4. boolean: true/false
+
+let flag = true;
+console.log(flag);
+console.log(typeof flag);
+
+let isElementExist = false;
+console.log(isElementExist);
+
+//5. null
+let obj = null;
+console.log(obj);
+console.log(typeof null); //type of null is object and it's a legacy bug in js
+
+
